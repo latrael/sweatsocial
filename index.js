8c534a97f0bff149b0e5dc9192a2645f1dd29669
Nabil <nabil@cu-biot-7-10.203.155.233.int.colorado.edu>
2024-03-02 11:37:22 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express"); // To build an application server or API
+const app = express();
+const { Pool } = require('pg');
+const pgp = require("pg-promise")(); // To connect to the Postgres DB from the node server
+const bodyParser = require("body-parser");
+const session = require("express-session"); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require("bcrypt"); //  To hash passwords
+const axios = require("axios"); // To make HTTP requests from our server. We'll learn more about it in Part B.
+app.use(express.static("src"));
+
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({ 
+  cloud_name: 'dzhqemlkr', 
+  api_key: '934727676964224', 
+  api_secret: 'pYMapPWdI3pStFxJP6MKeeJNpI4' 
+});
+// *****************************************************
+// <!-- Section 2 : Connect to DB -->
+// *****************************************************
+
+// database configuration
+const dbConfig = {
+  host: "db", // the database server
+  port: 5432, // the database port
+  database: process.env.POSTGRES_DB, // the database name
+  user: process.env.POSTGRES_USER, // the user account to connect with
+  password: process.env.POSTGRES_PASSWORD, // the password of the user account
+};
+
+const db = pgp(dbConfig);
+
+// test your database
+db.connect()
+  .then((obj) => {
+    console.log("Database connection successful"); // you can view this message in the docker compose logs
+    obj.done(); // success, release the connection;
+  })
+  .catch((error) => {
+    console.log("ERROR:", error.message || error);
+  });
+
+// *****************************************************
+// <!-- Section 3 : App Settings -->
+// *****************************************************
+
+app.set("view engine", "ejs"); // set the view engine to EJS
+app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+
+// initialize session variables
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// API ROUTES HERE
