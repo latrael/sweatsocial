bcf1fd304b7ef3fba1a40d80c7f6072df24eedae
Eldin Basic <elba8705@colorado.edu>
2024-03-02 13:32:31 -0700

    login and register start

--- index.js
+++ index.js
@@ -60,6 +60,14 @@ app.get("/", (req, res) => {
     res.render("pages/home");
   });
 
+  app.get("/login", (req, res) => {
+    res.render("pages/login");
+  });
+
+  app.get("/register", (req, res) => {
+    res.render("pages/register");
+  });
+
 
 
 
